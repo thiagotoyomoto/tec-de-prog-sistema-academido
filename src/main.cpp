8637bfb72c21a378c3c78bf120682626d36754b1
Thiago Toyomoto <T.K.Toyomoto@gmail.com>
2020-08-26 09:48:37 -0300

    Slide 1B - Simples

--- src/main.cpp
+++ src/main.cpp
@@ -6,32 +6,31 @@ struct Pessoa
     int mes;
     int ano;
     int idade;
-};
 
-void calcularIdade(struct Pessoa& pessoa, int diaAtual, int mesAtual,
-                   int anoAtual)
-{
-    pessoa.idade = anoAtual - pessoa.ano;
+    void calcularIdade(int diaAtual, int mesAtual, int anoAtual)
+    {
+        idade = anoAtual - ano;
 
-    if(pessoa.mes < mesAtual)
-    {
-        pessoa.idade -= 1;
-    }
-    else if(pessoa.mes == mesAtual)
-    {
-        if(pessoa.dia < diaAtual)
+        if(mes < mesAtual)
         {
-            pessoa.idade -= 1;
+            idade -= 1;
+        }
+        else if(mes == mesAtual)
+        {
+            if(dia < diaAtual)
+            {
+                idade -= 1;
+            }
         }
     }
-}
+};
 
 int main()
 {
     struct Pessoa einstein = {14, 3, 1879, 0}, newton = {4, 1, 1643, 0};
 
-    calcularIdade(einstein, 26, 8, 2020);
-    calcularIdade(newton, 26, 8, 2020);
+    einstein.calcularIdade(26, 8, 2020);
+    newton.calcularIdade(26, 8, 2020);
 
     printf("A idade de Einstein seria: %d\n", einstein.idade);
     printf("A idade de Newton seria: %d\n", newton.idade);
