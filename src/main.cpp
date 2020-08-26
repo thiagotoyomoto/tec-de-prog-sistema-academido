b96cea74bc898c5e51a067916fed533f592c9cb3
Thiago Toyomoto <T.K.Toyomoto@gmail.com>
2020-08-26 19:32:27 -0300

    Slide 2 - Cin e cout

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,23 @@ int main()
     Pessoa ninguem, eu("Thiago", 18, 4, 2001),
         einstein("Einstein", 14, 3, 1879), newton("Newton", 4, 1, 1643);
 
-    ninguem.calcularEImprimirIdade(26, 8, 2020);
-    eu.calcularEImprimirIdade(26, 8, 2020);
-    einstein.calcularEImprimirIdade(26, 8, 2020);
-    newton.calcularEImprimirIdade(26, 8, 2020);
+    int diaAtual, mesAtual, anoAtual;
+
+    std::cout << "Informe o dia atual: ";
+    std::cin >> diaAtual;
+
+    std::cout << "Informe o mes atual: ";
+    std::cin >> mesAtual;
+
+    std::cout << "Informe o ano atual: ";
+    std::cin >> anoAtual;
+
+    std::cout << '\n';
+
+    ninguem.calcularEImprimirIdade(diaAtual, mesAtual, anoAtual);
+    eu.calcularEImprimirIdade(diaAtual, mesAtual, anoAtual);
+    einstein.calcularEImprimirIdade(diaAtual, mesAtual, anoAtual);
+    newton.calcularEImprimirIdade(diaAtual, mesAtual, anoAtual);
 
     // Utilizar sempre o std::endl é custoso, então utilizar o '\n' para que
     // seja liberado o buffer todo de uma vez.
