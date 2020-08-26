e96366404fe4554c9cde3192e569469aaaa3ccf2
Thiago Toyomoto <T.K.Toyomoto@gmail.com>
2020-08-26 09:30:51 -0300

    Slide 1B - Versão 1

--- src/main.c
+++ src/main.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
 
+struct Pessoa
+{
+    int dia;
+    int mes;
+    int ano;
+    int idade;
+};
+
+int calcularIdade(struct Pessoa pessoa, int anoAtual)
+{
+    return anoAtual - pessoa.idade;
+}
+
 int main()
 {
-    printf("Hello World!\n");
+    struct Pessoa einstein = {14, 3, 1879, 0}, newton = {4, 1, 1643, 0};
 
-    getchar();
+    einstein.idade = calcularIdade(einstein, 2020);
+    newton.idade   = calcularIdade(newton, 2020);
+
+    printf("A idade de Einstein seria: %d\n", einstein.idade);
+    printf("A idade de Newton seria: %d\n", newton.idade);
+
     return 0;
 }
